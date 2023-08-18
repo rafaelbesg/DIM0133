af84a5b61967071c0473450c13ec6321943b7df8
Rafael <rafaelufrn@gmail.com>
2023-08-18 13:56:52 -0300

    Adicionado arquivo de exemplo01.c

---
+++ 01 introducao/exemplo01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    printf("Digite um inteiro n e eu direi o seu dobro: ");
+    scanf("%d", &n);
+    printf("O dobro é: %d\n", 2*n);
+
+    return 0;
+}
